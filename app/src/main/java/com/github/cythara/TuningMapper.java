9edcefc1488054c01b1cc3643054210ef64d89f7
gstraube <gstraube@mailbox.org>
2017-10-15 13:16:52 +0200

    Add Drop D tuning

--- app/src/main/java/com/github/cythara/TuningMapper.java
+++ app/src/main/java/com/github/cythara/TuningMapper.java
@@ -5,12 +5,15 @@ import android.util.Log;
 class TuningMapper {
 
     private static final int GUITAR_TUNING_POSITION = 0;
-    private static final int UKULELE_TUNING_POSITION = 1;
+    private static final int DROP_D_TUNING_POSITION = 1;
+    private static final int UKULELE_TUNING_POSITION = 2;
 
     static Tuning getTuningFromPosition(int position) {
         switch (position) {
             case GUITAR_TUNING_POSITION:
                 return new StandardTuning();
+            case DROP_D_TUNING_POSITION:
+                return new DropDTuning();
             case UKULELE_TUNING_POSITION:
                 return new UkuleleTuning();
             default:
