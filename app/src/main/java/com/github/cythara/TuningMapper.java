1b527cd3f28014296ee71c460f6f45015bf2f4c1
gstraube <gstraube@mailbox.org>
2018-11-01 18:08:18 +0100

    Set chromatic tuning as default (issue #20)

--- app/src/main/java/com/github/cythara/TuningMapper.java
+++ app/src/main/java/com/github/cythara/TuningMapper.java
@@ -16,20 +16,22 @@ import com.github.cythara.tuning.ViolinTuning;
 
 class TuningMapper {
 
-    private static final int GUITAR_TUNING_POSITION = 0;
-    private static final int DROP_D_TUNING_POSITION = 1;
-    private static final int DROP_C_TUNING_POSITION = 2;
-    private static final int DROP_C_SHARP_TUNING_POSITION = 3;
-    private static final int OPEN_G_TUNING = 4;
-    private static final int BASS_TUNING_POSITION = 5;
-    private static final int UKULELE_TUNING_POSITION = 6;
-    private static final int D_TUNING_POSITION = 7;
-    private static final int VIOLIN_TUNING_POSITION = 8;
-    private static final int CELLO_TUNING_POSITION = 9;
-    private static final int CHROMATIC_TUNING_POSITION = 10;
+    private static final int CHROMATIC_TUNING_POSITION = 0;
+    private static final int GUITAR_TUNING_POSITION = 1;
+    private static final int DROP_D_TUNING_POSITION = 2;
+    private static final int DROP_C_TUNING_POSITION = 3;
+    private static final int DROP_C_SHARP_TUNING_POSITION = 4;
+    private static final int OPEN_G_TUNING = 5;
+    private static final int BASS_TUNING_POSITION = 6;
+    private static final int UKULELE_TUNING_POSITION = 7;
+    private static final int D_TUNING_POSITION = 8;
+    private static final int VIOLIN_TUNING_POSITION = 9;
+    private static final int CELLO_TUNING_POSITION = 10;
 
     static Tuning getTuningFromPosition(int position) {
         switch (position) {
+            case CHROMATIC_TUNING_POSITION:
+                return new ChromaticTuning();
             case GUITAR_TUNING_POSITION:
                 return new GuitarTuning();
             case DROP_D_TUNING_POSITION:
@@ -50,8 +52,6 @@ class TuningMapper {
                 return new ViolinTuning();
             case CELLO_TUNING_POSITION:
                 return new CelloTuning();
-            case CHROMATIC_TUNING_POSITION:
-                return new ChromaticTuning();
             default:
                 Log.w("com.github.cythara", "Unknown position for tuning dropdown list");
                 return new GuitarTuning();
