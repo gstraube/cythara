e5fe42c330bbfdb225a0a4eaf32bfbf771d4be18
Romain Garbage <romain.garbage@gmail.com>
2020-07-31 21:40:19 +0300

    Add Turkish Oud standard tuning (F#C#BEAD)

---
+++ app/src/main/java/com/github/cythara/tuning/OudStdTurkish.java
@@ -0,0 +1,54 @@
+package com.github.cythara.tuning;
+
+import com.github.cythara.Note;
+import com.github.cythara.NoteName;
+import com.github.cythara.Tuning;
+
+import static com.github.cythara.NoteName.*;
+
+public class OudStdTurkishTuning implements Tuning {
+
+    @Override
+    public Note[] getNotes() {
+        return Pitch.values();
+    }
+
+    @Override
+    public Note findNote(String name) {
+        return Pitch.valueOf(name);
+    }
+
+    public enum Pitch implements Note {
+
+        C2_SHARP(C, 2, "#"),
+        F2_SHARP(F, 2, "#"),
+        B3(B, 3),
+        E3(E, 3),
+        A3(A, 3),
+        D4(D, 4);
+
+        private final String sign;
+        private final int octave;
+        private NoteName name;
+
+        Pitch(NoteName name, int octave) {
+            this.name = name;
+            this.octave = octave;
+            this.sign = "";
+        }
+
+        public NoteName getName() {
+            return name;
+        }
+
+        @Override
+        public int getOctave() {
+            return octave;
+        }
+
+        @Override
+        public String getSign() {
+            return sign;
+        }
+    }
+}
